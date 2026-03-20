d68fbec0915ea3087542543168b6dbcbf5b52bc1
ashna-verma <ashnaverma1706@gmail.com>
2026-03-20 21:12:24 +0530

    App Context & API endpoints

---
+++ src/util/apiEndpoints.js
@@ -0,0 +1,6 @@
+export const BASE_URL = "https://moneymantra.onrender.com/api/v1.0";
+
+export const API_ENDPOINTS = {
+    LOGIN: "/login",
+    REGISTER: "/register",
+}
---
+++ src/util/axiosConfig.jsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+//JavaScript library used to make HTTP requests from your frontend or backend.
+const axiosConfig = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+    }
+});
+
+//Interceptors so that we do not send out Bearer token everytime when making api calls
+//bearer tokens are not needed for login, register, health apis, we can exclude them
+
+const excludeEndpoints = ["/login", "/register", "/status", "/activate", "/health"];
+
+//request interceptor
+axiosConfig.interceptors.request.use((config) => {
+    const shouldSkipToken = excludeEndpoints.some((endpoint) => {
+        config.url?.includes(endpoint)      //boolean
+    });
+
+    if (!shouldSkipToken){
+        const accessToken = localStorage.getItem("token");
+        if (accessToken){
+            config.headers.Authorization= `Bearer ${accessToken}`;
+        }
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+//response interceptor
+axiosConfig.interceptors.response.use((response) => {
+    return response;
+}, (error) => {
+    if (error.response){
+        if (error.response.status === 401){
+            window.location.href = "/login";
+        }
+        else if (error.response.status === 500){
+            console.error("Server error. Please try again later");
+        }
+    }
+    else if (error.code === "ECONNABORTED"){        //connection aborted
+        console.error("Request timeout. Please try again.")
+    }
+    return Promise.reject(error);
+});
