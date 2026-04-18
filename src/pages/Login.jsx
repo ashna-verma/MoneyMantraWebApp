bce1669c90f583b7db6184cea4d9b42291eca7b7
ashna-verma <ashnaverma1706@gmail.com>
2026-04-18 18:27:17 +0530

    Login module setup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,69 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import {assets} from "../assets/assets.js";
 import Input from "../components/Input.jsx";
+import { validateEmail } from "../util/validation.js";
+import axiosConfig from "../util/axiosConfig.jsx";
+import { Loader, LoaderCircle } from "lucide-react";
+import toast from "react-hot-toast";
+import { API_ENDPOINTS } from "../util/apiEndpoints.js";
+import { AppContext } from "../context/AppContext.jsx";
 
 const Login = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const {setUser} = useContext(AppContext);
 
     const navigate= useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+
+        //basic validation
+        if (!validateEmail(email)){
+            setError("Please enter a valid email address");
+            setIsLoading(false);
+            return;
+        }
+        if (!password.trim()){
+            setError("Please enter your password");
+            setIsLoading(false);
+            return;
+        }
+
+        setError("");
+
+        //LOGIN API call
+        try{
+            const response = await axiosConfig.post(API_ENDPOINTS.LOGIN, {
+                email,
+                password
+            });
+            const {token, user} = response.data;
+            if (token){
+                localStorage.setItem("token", token);
+                setUser(user);
+                navigate("/dashboard");
+            }
+        }
+        catch(error){
+            if (error.response && error.response.data.message){
+                setError(error.response.data.message);
+            }
+            else{
+                console.error('Something went wrong', error);
+                setError(error.message);
+            }
+        }
+        finally{
+            setIsLoading(false);
+        }
+    }
+
     return (
         <div className= "h-screen w-full relative flex items-center justify-center overflow-hidden">
             {/*Background image with blur */}
@@ -26,9 +80,9 @@ const Login = () => {
                         Please enter your details to log in
                     </p>
 
-                    <form className="space-y-4">
+                    <form onSubmit={handleSubmit} className="space-y-4">
                         
-                        {/*Creating a reuable component*/}
+                        {/*Creating a reusable component*/}
 
                         <Input
                             value={email}
@@ -51,8 +105,15 @@ const Login = () => {
                             </p>
                         )}
 
-                        <button className="btn-primary w-full py-3 text-lg font-medium hover:text-primary-dark transition-colors" type="submit">
-                            LOGIN
+                        <button disabled={isLoading} className={`btn-primary w-full py-3 text-lg font-medium flex items-center justify-center gap-2 ${isLoading ? 'opacity-60 cursor-not-allowed' : ''}`} type="submit">
+                            {isLoading ? (
+                                <>
+                                <LoaderCircle className="animate-spin w-5 h-5"/>
+                                Logging in...
+                                </>
+                            ) : (
+                                "LOGIN"
+                            )}
                         </button>
 
                         <p className="text-sm text-slate-800 text-center mt-6">
