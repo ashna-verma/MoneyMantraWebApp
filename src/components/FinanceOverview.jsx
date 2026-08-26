43695aa702c384bbdba7cf18a143f0261de59d82
ashna-verma <ashnaverma1706@gmail.com>
2026-08-26 21:04:34 +0530

    Dashboard component and Data

---
+++ src/components/FinanceOverview.jsx
@@ -0,0 +1,29 @@
+import { addThousandsSeparator } from "../util/util";
+import CustomPieChart from "./CustomPieChart";
+
+const FinanceOverview = ({totalBalance, totalIncome, totalExpense}) => {
+    const COLORS = ["#59168B", "#a0090e", "#016630"];
+    
+    const balanceData = [
+        {name: "Total Balance", amount: totalBalance},
+        {name: "Total Expense", amount: totalExpense},
+        {name: "Total Income", amount: totalIncome},
+    ]
+    return (
+        <div className="card p-4">
+            <div className="flex items-center justify-between">
+                <h5 className="text-lg">Financial Overview</h5>
+            </div>
+
+            <CustomPieChart
+                data= {balanceData}
+                label="Total Balance"
+                totalAmount= {`₹${addThousandsSeparator(totalBalance)}`}
+                colors= {COLORS}
+                showTextAnchor
+            />
+        </div>
+    )
+}
+
+export default FinanceOverview;
--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -22,5 +22,6 @@ export const API_ENDPOINTS = {
     EXPENSE_EXCEL_DOWNLOAD: "/expenses/excel/download",
     EXPENSE_EMAIL: "/expenses/email",
     APPLY_FILTER: "/filter",
+    DASHBOARD_DATA: "/dashboard",
     UPLOAD_IMAGE: `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`
 }
